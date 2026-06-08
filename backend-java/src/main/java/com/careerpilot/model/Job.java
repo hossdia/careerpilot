c18db14a4d4ab5f7dac6b12e610e8b5a7f364199
Amina Rahman <aminarahmanshalu@gmail.com>
2026-06-08 22:07:40 +0600

    Create Job.java

---
+++ backend-java/src/main/java/com/careerpilot/model/Job.java
@@ -0,0 +1,10 @@
+package com.careerpilot.model;
+
+import java.util.List;
+
+public class Job {
+    public String title;
+    public String company;
+    public String location;
+    public List<String> skills;
+}
