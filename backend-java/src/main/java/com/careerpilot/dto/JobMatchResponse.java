00feb6a8638ba1fa617da428c528b18b4813ed0f
Amina Rahman <aminarahmanshalu@gmail.com>
2026-06-08 22:10:42 +0600

    Create JobMatchResponse.java

---
+++ backend-java/src/main/java/com/careerpilot/dto/JobMatchResponse.java
@@ -0,0 +1,9 @@
+package com.careerpilot.dto;
+
+public class JobMatchResponse {
+    public String title;
+    public String company;
+    public String location;
+    public int fitScore;
+    public String reason;
+}
